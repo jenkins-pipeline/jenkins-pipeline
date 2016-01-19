724a256b8ce10457c1b0952524ed267a1b02f163
Vinicius Gama <vgama@thoughtworks.com>
2016-01-19 19:17:22 -0200

    adds logic to identify if downstream jobs have already run

--- app.rb
+++ app.rb
@@ -2,36 +2,53 @@ require 'sinatra'
 require 'httparty'
 require 'tilt/erb'
 require 'yaml'
-
-USERNAME = ENV["MONITOR_USERNAME"]
-TOKEN = ENV["MONITOR_TOKEN"]
-JENKINS_BASE_URL = ENV['MONITOR_JENKINS_BASE_URL']
+require './app/jenkins_client'
 
 get '/' do
+  client = JenkinsClient.new
   @pipelines = Dir["config/*.yml"].map do |file|
     pipeline = YAML.load_file(file)
-    get_jobs_from(pipeline)
+    jobs_in_folder = client.all_jobs(pipeline)
+    create_pipeline(jobs_in_folder, pipeline)
  end
 
   erb :index
 end
 
-def get_jobs_from pipeline
-  auth = {username: USERNAME, password: TOKEN}
+def create_pipeline(jobs_in_folder, pipeline)
   jobs = []
 
-  jenkins_base_url = URI.escape("https://#{JENKINS_BASE_URL}/job/#{pipeline[:folder]}-Folder/api/json?tree=jobs[name,builds[number,result]{0,1}]")
-  jobs_in_folder = JSON.parse(HTTParty.get(jenkins_base_url, basic_auth: auth).body)
-
-  puts jobs_in_folder.inspect
-
+  has_incomplete = false
   pipeline[:jobs].each do |job|
-    job_name = "#{pipeline[:folder]}-#{job[:ci_name]}"
-    result = jobs_in_folder["jobs"].select {|job_in_folder| job_name == job_in_folder["name"] }.first
+    ran = 100
+    job_in_folder = jobs_in_folder["jobs"].select {|job_in_folder| job[:ci_name] == job_in_folder["name"] }.first
+    result = job_in_folder["lastCompletedBuild"]["result"].downcase
+
+    upstream_job = jobs.select {|j| j[:ci_name] == job[:upstream]}.first
+    upstream_in_folder = job_in_folder["upstreamProjects"].select {|upstream| upstream["name"] == job[:upstream]}.first
+
+    if (not upstream_job.nil?)
+      current_upstream_build = upstream_job[:number]
+      upstream_in_folder_build = upstream_in_folder["nextBuildNumber"] - 1
+
+      if (current_upstream_build != upstream_in_folder_build || has_incomplete == true)
+        result = "not"
+        ran = 0
+        has_incomplete = true
+      end
+    end
+
+    if (job_in_folder["lastBuild"]["building"] == true)
+      result = "warning progress-bar-striped active"
+    end
+
     jobs.push({
-      	:name => job[:name],
-        :last_build => result["builds"].first["result"].downcase
-      })
+    	:name => job[:name],
+      :ci_name => job[:ci_name],
+      :ran => ran,
+      :number => job_in_folder["lastCompletedBuild"]["number"],
+      :last_build => result
+    })
   end
 
   {
---
+++ app/jenkins_client.rb
@@ -0,0 +1,15 @@
+require 'httparty'
+
+USERNAME = ENV["MONITOR_USERNAME"]
+TOKEN = ENV["MONITOR_TOKEN"]
+JENKINS_BASE_URL = ENV['MONITOR_JENKINS_BASE_URL']
+
+class JenkinsClient
+  def all_jobs pipeline
+    auth = {username: USERNAME, password: TOKEN}
+
+    jenkins_base_url = URI.escape("https://#{JENKINS_BASE_URL}/job/#{pipeline[:folder]}-Folder/api/json?tree=jobs[name,upstreamProjects[name,nextBuildNumber],lastBuild[building],lastCompletedBuild[number,duration,timestamp,result]]")
+
+    JSON.parse(HTTParty.get(jenkins_base_url, basic_auth: auth).body)
+  end
+end
