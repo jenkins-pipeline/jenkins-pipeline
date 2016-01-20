f12893adb07722d2c647799cc86f5b20b539de3a
Vinicius Gama <vgama@thoughtworks.com>
2016-01-20 00:21:42 -0200

    condition for endpoint url depending if its a folder or not

--- app/jenkins_client.rb
+++ app/jenkins_client.rb
@@ -10,8 +10,13 @@ JENKINS_BASE_URL = ENV['MONITOR_JENKINS_BASE_URL']
 class JenkinsClient
   def all_jobs pipeline
     auth = {username: USERNAME, password: TOKEN}
+    jenkins_base_url = ""
 
-    jenkins_base_url = URI.escape("https://#{JENKINS_BASE_URL}/job/#{pipeline[:folder]}-Folder/api/json?tree=jobs[name,upstreamProjects[name,nextBuildNumber],lastBuild[building],lastCompletedBuild[number,duration,timestamp,result]]")
+    if (not pipeline[:folder].nil?)
+      jenkins_base_url = URI.escape("https://#{JENKINS_BASE_URL}/job/#{pipeline[:folder]}/api/json?tree=jobs[name,upstreamProjects[name,nextBuildNumber],lastBuild[building],lastCompletedBuild[number,duration,timestamp,result]]")
+    else
+      jenkins_base_url = URI.escape("https://#{JENKINS_BASE_URL}/api/json?tree=jobs[name,upstreamProjects[name,nextBuildNumber],lastBuild[building],lastCompletedBuild[number,duration,timestamp,result]]")
+    end
 
     JSON.parse(HTTParty.get(jenkins_base_url, basic_auth: auth).body)
   end
