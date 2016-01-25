4aab5b817ffffbb08d0f50fe2770108564c787e2
Junior Ales <edilson.ales.jr@gmail.com>
2016-01-25 00:11:56 -0200

    listing pipelines as cards and adding preloader

--- client/app/views/components/pipelineView.js
+++ client/app/views/components/pipelineView.js
@@ -1,24 +1,54 @@
 'use strict';
 
+var _ = require('lodash');
 var $ = require('jquery');
 
+// $divider :: undefined -> DOM
+var $divider = function() {
+  return $('<div>', { 'class': 'divider' });
+};
+
+// $basePipelineMarkup :: undefined -> DOM
+var $basePipelineMarkup = function() {
+  return $('<section>', { 'class': 'pipeline-container row' });
+};
+
 // $renderTitle :: String -> DOM
-var $renderTitle = function(pipelineName) {
-  return $('<h1>', { text: pipelineName, 'class': 'title col s12' });
+var $renderTitle = function(name) {
+  return $('<header>', { 'class': 'row' }).
+    append($('<h4>', { text: name, 'class': 'title col s12' }));
 };
 
-// $render :: Pipeline -> DOM
-var $render = function(pipeline) {
-  var $container = $('<section>', { 'class': 'pipeline-container section' });
-  $container.append($renderTitle(pipeline.name));
-  $container.append($('<div>', { 'class': 'divider' }));
-
-  return $container;
+// $renderJob :: Job -> DOM
+var $renderJob = function(pipelineJob) {
+  return $('<article>', { 'class': 'job-card col card' }).
+    append($('<div>', { 'class': 'title card-content', text: pipelineJob.name }));
 };
+
+// $renderJobs :: [Job] -> DOM
+var $renderJobs = function(jobs) {
+  return $('<section>', { 'class': 'row' }).append(jobs.map($renderJob));
+};
+
+// $toPipelineDOM :: Pipeline -> PipelineDOM
+var $toPipelineDOM = function(pipeline) {
+  return {
+    title: $renderTitle(pipeline.name),
+    jobs: $renderJobs(pipeline.jobs)
+  };
+};
+
+// $wrapMarkup :: (undefined -> DOM) -> (undefined -> DOM) -> PipelineDOM -> DOM
+var $wrapMarkup = _.curry(function($header, $footer, pipelineDOM) {
+  return $header().
+    append(pipelineDOM.title).
+    append(pipelineDOM.jobs).
+    append($footer());
+});
 
 var pipelineView = {
   // $render :: Pipeline -> DOM
-  $render: $render
+  $render: _.flow($toPipelineDOM, $wrapMarkup($basePipelineMarkup, $divider))
 };
 
 module.exports = pipelineView;
--- client/app/views/index.js
+++ client/app/views/index.js
@@ -1,14 +1,18 @@
 'use strict';
 
-var _               = require('lodash');
-var h               = require('../helper/helpers');
+var _ = require('lodash');
+var $ = require('jquery');
+var h = require('../helper/helpers');
 var $renderPipeline = require('./components/pipelineView').$render;
-
-// renderView :: [Pipeline] -> undefined
-var renderView = _.flow(h.map($renderPipeline), h.$setHTML('#content-container'));
 
 // url :: undefined -> String
 var url = function() { return '/api'; };
+
+// $hideLoader :: void
+var $hideLoader = function() { $('.progress').hide(); };
+
+// renderView :: [Pipeline] -> undefined
+var renderView = _.flow(h.map($renderPipeline), h.$setHTML('#content-container'), $hideLoader);
 
 module.exports = {
   // init :: void
