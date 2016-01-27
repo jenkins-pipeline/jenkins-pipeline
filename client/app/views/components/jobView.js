362caaffa5fc532c814f9c9d931191536d421c38
Junior Ales <edilson.ales.jr@gmail.com>
2016-01-27 20:11:37 -0200

    Revealing large job names that do not fit in the job card on hover

--- client/app/views/components/jobView.js
+++ client/app/views/components/jobView.js
@@ -4,7 +4,7 @@
 var renderJob = function(job) {
   return '<article class="job-card col s12 m2 card">' +
            '<div class="title-wrapper card-content truncate">' +
-             '<span class="title card-title">' + job.name + '</span>' +
+             '<span title="'+ job.name +'" class="title card-title">'+ job.name +'</span>' +
            '</div>' +
            '<div class="status card-action" data-status="' + job.last_build + '">' +
              '<div class="loading progress" data-job-running="' + !job.ran + '">' +
