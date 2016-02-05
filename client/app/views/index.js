409dfd16546b68a59407d05b001409aac921cfe8
Junior Ales <edilson.ales.jr@gmail.com>
2016-02-05 00:22:53 -0200

    fetching pipelines async

--- client/app/views/index.js
+++ client/app/views/index.js
@@ -1,20 +1,44 @@
 'use strict';
 
-var _ = require('lodash');
 var h = require('../helpers/helpers');
 var renderPipeline = require('./components/pipelineView').render;
 
-// url :: undefined -> String
-var url = function() { return '/api/pipelines'; };
+var pipelineNames = function pipelineNames() {
+  return Promise.resolve([
+    'Digital Service',
+    'Transversal Service',
+    'Web Application'
+  ]);
+};
 
-// renderView :: [Pipeline] -> undefined
-var renderView = _.flow(
-  h.map(renderPipeline),
-  h.$setHTML('#content-container'),
-  h.$hide('.pipelines-loading')
-);
+var $fetchPipeline = function fetchPipeline(pipelineName) {
+  return h.$getJSON('/api/pipelines/' + pipelineName);
+};
+
+var appendPipelineView = function appendPipelineView(pipeline) {
+  h.$append('#content-container', renderPipeline(pipeline));
+};
+
+var renderPipelinesReducer = function renderPipelinesReducer(promises, pipelinePromise) {
+  return promises.then(function() {
+    return pipelinePromise;
+  }).then(appendPipelineView);
+};
+
+var appendError = function appendError(err) {
+  h.$append('#content-container', 'Fetching pipelines has a problem. Please try again');
+  h.trace(err);
+};
+
+var hideLoader = function hideLoader() {
+  h.$hide('.pipelines-loading');
+};
 
 module.exports = {
-  // init :: void
-  init: _.flow(url, h.$getJSON(renderView))
+  init: function init() {
+    pipelineNames().then(function resolvedPipelineNames(pipelineNames) {
+      return pipelineNames.map($fetchPipeline).
+        reduce(renderPipelinesReducer, Promise.resolve());
+    }).catch(appendError).then(hideLoader);
+  }
 };
