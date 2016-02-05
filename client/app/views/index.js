4e331c9db1b6fb48594bc2b4b0964e88e3567686
Junior Ales <edilson.ales.jr@gmail.com>
2016-02-05 18:02:21 -0200

    renaming functions and fixing an error handling

--- client/app/views/index.js
+++ client/app/views/index.js
@@ -4,48 +4,48 @@ var _ = require('lodash');
 var h = require('../helpers/helpers');
 var renderPipeline = require('./components/pipelineView').render;
 
-var appendError = function(message) {
+function appendError(message) {
   var errorMsg = message ||  'An error occurred loading the page. Please try again.';
-  return function appendError(err) {
+  return function _appendError(err) {
     h.$append('#content-container', errorMsg);
     h.trace(err);
   };
-};
+}
 
-var fetchPipelinesAsync = function fetchPipelinesAsync() {
-  var pipelineNames = function pipelineNames() {
+function hideLoader() {
+  h.$hide('.pipelines-loading');
+}
+
+function fetchPipelinesAsync() {
+  function pipelineNames() {
     return Promise.resolve([
       'Web Application',
       'Digital Service',
       'Transversal Service'
     ]);
-  };
+  }
 
-  var renderPipelineView = function renderPipelineView(pipeline) {
+  function renderPipelineView(pipeline) {
     h.$append('#content-container', renderPipeline(pipeline));
-  };
+  }
 
-  var hideLoader = function hideLoader() {
-    h.$hide('.pipelines-loading');
-  };
-
-  var $fetchPipeline = function(pipelineName) {
+  function $fetchPipeline(pipelineName) {
     var errorMsg = 'Fetching Pipeline "' + pipelineName + '" failed. Please refresh the page.';
 
     return h.$getJSON('/api/pipelines/' + pipelineName).
              then(renderPipelineView).
              catch(appendError(errorMsg));
-  };
+  }
 
   return function asyncInit() {
     pipelineNames().then(function resolvedPipelineNames(pipelineNames) {
       return pipelineNames.map($fetchPipeline);
     }).catch(appendError()).then(hideLoader);
   };
-};
+}
 
 // use this until https://github.com/jenkins-pipeline/jenkins-pipeline/issues/31 is done
-var fetchAllPipelinesAtOnce = function fetchAllPipelinesAtOnce() {
+function fetchAllPipelinesAtOnce() {
   var renderView = _.flow(
     h.map(renderPipeline),
     h.$setHTML('#content-container')
@@ -54,10 +54,10 @@ var fetchAllPipelinesAtOnce = function fetchAllPipelinesAtOnce() {
   return function init() {
     h.$getJSON('/api/pipelines').
       then(renderView).
-      catch(appendError).
-      then(function() { h.$hide('.pipelines-loading'); });
+      catch(appendError()).
+      then(hideLoader);
   };
-};
+}
 
 module.exports = {
   init: h.env(location) === 'dev' ? fetchPipelinesAsync() : fetchAllPipelinesAtOnce()
