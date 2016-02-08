14a03399c647f2ac81828c690a2b89f7da2dd5b1
Thiago Xavier <thiagoxvo@gmail.com>
2016-02-08 14:01:21 -0200

    Renames test and removes unecessary comment

--- spec/jenkins_pipeline/app_spec.rb
+++ spec/jenkins_pipeline/app_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
 describe JenkinsPipeline::App do
-
-  it "Returns urls from all pipeline config files" do
-    get '/api/pipelines/ids'
-    # expect(subject).to have_received(:configuration)
-    expect(last_response.header["Content-Type"]).to eq "application/json"
-    expect(last_response).to be_ok
+  context "Smoke test" do
+    it "Assert /api/pipelines/ids the endpoint is working" do
+      get '/api/pipelines/ids'
+      expect(last_response.header["Content-Type"]).to eq "application/json"
+      expect(last_response).to be_ok
+    end
   end
-
 end
