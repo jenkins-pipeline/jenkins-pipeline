33319b4f852d4bc5e4faa6aa69d5c7c612610dcd
Vinicius Gama <vinicius.sgama@gmail.com>
2016-02-09 17:04:23 -0200

    renaming method and making it private

--- lib/jenkins_pipeline/pipeline_builder.rb
+++ lib/jenkins_pipeline/pipeline_builder.rb
@@ -7,21 +7,21 @@ module JenkinsPipeline
       pipeline["jobs"].each_with_index do |job, index|
         matching_job = find_job(job["ci_name"], jenkins_jobs["jobs"])
         job_instance = Job.new(matching_job, job["name"])
-        pipeline_instance.add_job job_instance, is_first?(index)
+        pipeline_instance.add_job job_instance, first?(index)
         jobs << job_instance
       end
 
       pipeline_instance
     end
 
-    def is_first?(index)
-      index == 0
-    end
-
     private
 
     def find_job(name, jobs)
       jobs.find { |job| job["name"] == name }
+    end
+
+    def first?(index)
+      index == 0
     end
   end
 end
