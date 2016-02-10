d829ac8c59a71525a452743e416b6f88d139e7d4
Thiago Xavier <thiagoxvo@gmail.com>
2016-02-10 16:19:49 -0200

    Sets the proper configuration to make Sinatra works with Docker

--- lib/jenkins_pipeline/app.rb
+++ lib/jenkins_pipeline/app.rb
@@ -2,6 +2,7 @@ module JenkinsPipeline
   class App < Sinatra::Base
 
     set :public_folder, './public'
+    set :bind, '0.0.0.0'
 
     get '/' do
       send_file File.join(settings.public_folder, 'index.html')
