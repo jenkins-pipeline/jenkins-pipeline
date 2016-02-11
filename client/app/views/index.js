1e41e629d0b8bb0af0fba27c32d80a86993cd054
Junior Ales <edilson.ales.jr@gmail.com>
2016-02-11 14:22:10 -0200

    fetching pipeline names from the endpoint and removing feature toggle

--- client/app/views/index.js
+++ client/app/views/index.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var _ = require('lodash');
 var h = require('../helpers/helpers');
 var renderPipeline = require('./components/pipelineView').render;
 
@@ -8,59 +7,36 @@ function appendError(message) {
   var errorMsg = message ||  'An error occurred loading the page. Please try again.';
   return function _appendError(err) {
     h.$append('#content-container', errorMsg);
-    h.trace(err);
+    console.log(err);
   };
 }
 
 function hideLoader() { h.$hide('.pipelines-loading'); }
 
-function fetchPipelinesAsync() {
-  function pipelineNames() {
-    return Promise.resolve([
-      'Web Application',
-      'Digital Service',
-      'Transversal Service'
-    ]);
-  }
-
-  function renderPipelines(pipeline) {
-    h.$append('#content-container', renderPipeline(pipeline));
-  }
-
-  function $fetchPipeline(name) {
-    var errorMsg = 'Fetching Pipeline "' + name + '" failed. Please refresh the page.';
-
-    return h.$getJSON('/api/pipelines/' + name).
-             then(renderPipelines).
-             catch(appendError(errorMsg));
-  }
-
-  function $fetchPipelines() {
-    return pipelineNames().then(function(names) {
-      return Promise.all(names.map($fetchPipeline));
-    });
-  }
-
-  return function asyncInit() {
-    $fetchPipelines().catch(appendError()).then(hideLoader);
-  };
+function $fetchPipelineNames() {
+  return h.$getJSON('/api/pipelines/ids').catch(appendError());
 }
 
-// use this until https://github.com/jenkins-pipeline/jenkins-pipeline/issues/31 is done
-function fetchAllPipelinesAtOnce() {
-  var renderView = _.flow(
-    h.map(renderPipeline),
-    h.$setHTML('#content-container')
-  );
+function renderPipelines(pipeline) {
+  h.$append('#content-container', renderPipeline(pipeline));
+}
 
-  return function init() {
-    h.$getJSON('/api/pipelines').
-      then(renderView).
-      catch(appendError()).
-      then(hideLoader);
-  };
+function $fetchPipeline(name) {
+  var errorMsg = 'Fetching Pipeline "' + name + '" failed. Please refresh the page.';
+
+  return h.$getJSON('/api/pipelines/' + name).
+    then(renderPipelines).
+    catch(appendError(errorMsg));
+}
+
+function $fetchPipelines() {
+  return $fetchPipelineNames().then(function(names) {
+    return Promise.all(h.map($fetchPipeline, names));
+  });
 }
 
 module.exports = {
-  init: h.env(location) === 'dev' ? fetchPipelinesAsync() : fetchAllPipelinesAtOnce()
+  init: function() {
+    $fetchPipelines().catch(appendError()).then(hideLoader);
+  }
 };
