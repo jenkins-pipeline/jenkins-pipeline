ebdb8d5de605e42c579f67ef66a063b247ff8e6a
Thiago Xavier <thiagoxvo@gmail.com>
2016-02-14 20:56:17 -0200

    One more time Hound suggestions

--- spec/jenkins_pipeline/jenkins_client_spec.rb
+++ spec/jenkins_pipeline/jenkins_client_spec.rb
@@ -15,8 +15,8 @@ describe JenkinsPipeline::JenkinsClient do
   describe "#all_jobs_from" do
     context "a pipeline with a folder" do
       it "request the right path" do
-        stub = stub_request(:get,  "user:token@jenkins-ci.com/api/json?tree=jobs%5Bname,lastBuild%5Bbuilding%5D,lastCompletedBuild%5Bnumber,duration,timestamp,result,changeSet%5Brevisions%5Brevision%5D%7B0,1%7D%5D%5D%5D")
-          .to_return(status: 200, body: "{}")
+        stub = stub_request(:get, "user:token@jenkins-ci.com/api/json?tree=jobs%5Bname,lastBuild%5Bbuilding%5D,lastCompletedBuild%5Bnumber,duration,timestamp,result,changeSet%5Brevisions%5Brevision%5D%7B0,1%7D%5D%5D%5D").to_return(status: 200, body: "{}")
+        
         subject.all_jobs_from pipeline
         expect(stub).to have_been_requested
       end
@@ -25,8 +25,8 @@ describe JenkinsPipeline::JenkinsClient do
     context "a pipwline without a folder" do
       let(:folder) { "folder" }
       it "request the right path" do
-        stub = stub_request(:get,  "user:token@jenkins-ci.com/job/#{folder}/api/json?tree=jobs%5Bname,lastBuild%5Bbuilding%5D,lastCompletedBuild%5Bnumber,duration,timestamp,result,changeSet%5Brevisions%5Brevision%5D%7B0,1%7D%5D%5D%5D")
-          .to_return(status: 200, body: "{}")
+        stub = stub_request(:get,  "user:token@jenkins-ci.com/job/#{folder}/api/json?tree=jobs%5Bname,lastBuild%5Bbuilding%5D,lastCompletedBuild%5Bnumber,duration,timestamp,result,changeSet%5Brevisions%5Brevision%5D%7B0,1%7D%5D%5D%5D").to_return(status: 200, body: "{}")
+
         subject.all_jobs_from pipeline
         expect(stub).to have_been_requested
       end
