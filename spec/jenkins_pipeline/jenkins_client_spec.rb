4bceaab22079042f51c52f2204f3dd890cf48d9f
Thiago Xavier <thiagoxvo@gmail.com>
2016-02-15 13:34:11 -0200

    Fix test names

--- spec/jenkins_pipeline/jenkins_client_spec.rb
+++ spec/jenkins_pipeline/jenkins_client_spec.rb
@@ -13,16 +13,16 @@ describe JenkinsPipeline::JenkinsClient do
   end
 
   describe "#all_jobs_from" do
-    context "a pipeline with a folder" do
+    context "a pipeline without a folder" do
       it "request the right path" do
         stub = stub_request(:get, "user:token@jenkins-ci.com/api/json?tree=jobs%5Bname,lastBuild%5Bbuilding%5D,lastCompletedBuild%5Bnumber,duration,timestamp,result,changeSet%5Brevisions%5Brevision%5D%7B0,1%7D%5D%5D%5D").to_return(status: 200, body: "{}")
-        
+
         subject.all_jobs_from pipeline
         expect(stub).to have_been_requested
       end
     end
 
-    context "a pipwline without a folder" do
+    context "a pipwline with a folder" do
       let(:folder) { "folder" }
       it "request the right path" do
         stub = stub_request(:get,  "user:token@jenkins-ci.com/job/#{folder}/api/json?tree=jobs%5Bname,lastBuild%5Bbuilding%5D,lastCompletedBuild%5Bnumber,duration,timestamp,result,changeSet%5Brevisions%5Brevision%5D%7B0,1%7D%5D%5D%5D").to_return(status: 200, body: "{}")
